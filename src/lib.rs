acd10c4e94602b90cee1617e871deddbce2a20d5
Iuri <iuri.granjeiro2@gmail.com>
2026-09-04 09:14:18 -0300

    Added documentation to Inv trait

--- src/lib.rs
+++ src/lib.rs
@@ -197,7 +197,33 @@ pub trait Inv
 where
     Self: std::marker::Sized,
 {
-    /// Inverts a state, possibly fallibly
+    /// Inverts a state multiplicatively, possibly fallibly
+    ///
+    /// # Exemples
+    ///
+    /// ```
+    /// use rubiks::Inv;
+    /// #[derive(Debug, Clone, PartialEq)]
+    /// enum FieldZ2 {
+    ///     Zero,
+    ///     One,
+    /// }
+    /// impl std::ops::Mul for FieldZ2 {
+    ///     type Output = Self;
+    ///     fn mul(self, rhs: Self) -> Self {
+    ///         let product_table = [[FieldZ2::Zero, FieldZ2::One], [FieldZ2::One, FieldZ2::Zero]];
+    ///         product_table[self as usize][rhs as usize].clone()
+    ///     }
+    /// }
+    /// impl Inv for FieldZ2 {
+    ///     fn inverse(&self) -> Self {
+    ///         self.clone()
+    ///     }
+    /// }
+    ///
+    /// assert_eq!(FieldZ2::Zero, FieldZ2::Zero * FieldZ2::Zero.inverse());
+    /// assert_eq!(FieldZ2::Zero, FieldZ2::One * FieldZ2::One.inverse());
+    /// ```
     #[must_use = "this returns the inverse of a state, without modifying the original state"]
     fn inverse(&self) -> Self;
 }
