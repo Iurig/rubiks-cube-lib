8701000627fc8d1342d2e5b45b18af8a08666f16
Iuri <iuri.granjeiro2@gmail.com>
2026-09-11 15:24:59 -0300

    Simple lib reorder.

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,7 @@ mod piece;
 pub mod zn;
 
 pub use cube3by3::Cube3By3;
+pub use cube3by3::moves::{ParseMoveError, ParseSequenceError};
 pub use cube3by3::pieces::{Center, Corner, Edge};
 pub use ops::{Inv, Pow};
 pub use piece::{Piece, PieceConfiguration};
-
-pub use cube3by3::moves::{ParseMoveError, ParseSequenceError};
