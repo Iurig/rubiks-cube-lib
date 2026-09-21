d4f150fa2b75b7d8e8d020ef1640c730379bbaf4
Iuri <iuri.granjeiro2@gmail.com>
2026-09-21 09:08:09 -0300

    created puzzles module, moved cube3by3 to it

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 #![doc = include_str!("../README.md")]
-mod cube3by3;
 mod ops;
 mod piece;
+mod puzzles;
 pub mod zn;
 
-pub use cube3by3::Cube3By3;
-pub use cube3by3::moves::{ParseMoveError, ParseSequenceError};
-pub use cube3by3::pieces::{Center, Corner, Edge};
 pub use ops::{Inv, Pow};
 pub use piece::{Piece, PieceConfiguration};
+pub use puzzles::cube3by3::Cube3By3;
+pub use puzzles::cube3by3::moves::{ParseMoveError, ParseSequenceError};
+pub use puzzles::cube3by3::pieces::{Center, Corner, Edge};
